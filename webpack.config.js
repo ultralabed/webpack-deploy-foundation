957e5f74a9fc94afa36468175ac207815a02a0d5
ed <ultralabed@gmail.com>
2017-03-28 12:07:35 +0800

    setup(webpack): set up webpack for builds

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,40 @@
 var webpack = require('webpack');
 var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const VENDOR_LIBS = [
+  'faker', 'lodash', 'react', 'react-dom', 'react-input-range', 'react-redux',
+  'react-router', 'redux', 'redux-form', 'redux-thunk'
+];
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    bundle: './src/index.js',
+    vendor: VENDOR_LIBS,
+  },
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
-  }
+    filename: '[name].[chunkhash].js',
+  },
+  module: {
+    rules: [
+      {
+        use: 'babel-loader',
+        test: /\.js$/, // Look for .js in other folders.
+        exclude: /node_modules/, //don't by to babel node_modules codes, waste of resources.
+      },
+      {
+        use: ['style-loader', 'css-loader'],
+        test: /.css$/,
+      }
+    ],
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({  //extract same modules from bundle to vendor
+      names: ['vendor', 'manifest']
+    }),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+    })
+  ],
 };
